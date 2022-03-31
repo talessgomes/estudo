46f598bc96cc8f23a4b1f5a3ebf697ea89e98f9d
talessgomes <tales.gomes_sl@hotmail.com>
2022-03-31 07:17:32 -0300

    novas-atividades-concluidas

--- ws-eclipse/Seção 08/ResolverProblema/MaiorTriangulo/src/Main.java
+++ ws-eclipse/Seção 08/ResolverProblema/MaiorTriangulo/src/Main.java
@@ -1,30 +1,41 @@
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Triangle;
+
 public class Main {
 
 	public static void main(String[] args) {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		double xA, xB, xC, yA, yB, yC; 
-		
+
+		Triangle x, y;
+		x = new Triangle();
+		y = new Triangle();
 		
 		System.out.println("Enter the measures of triangle X: ");
-		xA = sc.nextDouble();
-		xB = sc.nextDouble();
-		xC = sc.nextDouble();
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
 		
 		System.out.println("Enter the measures of triangle Y: ");
-		yA = sc.nextDouble();
-		yB = sc.nextDouble();
-		yC = sc.nextDouble();
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
 		
-		double p = (xA + xB + xC) / 2.0;
-		double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+		double areaX = x.area();
 		
-		double p = (yA + yB + yC) / 2.0;
-		double areaX = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+		double areaY = y.area();
+		 
+		System.out.printf("Triangle X area: %.4f%n", areaX);
+		System.out.printf("Triangle Y area: %.4f%n", areaY);
+		
+		if(areaX > areaY) {
+			System.out.println("Larger area: X");
+		} else {
+			System.out.println("Larger area: Y");
+		}
 		
 		
 		sc.close();
