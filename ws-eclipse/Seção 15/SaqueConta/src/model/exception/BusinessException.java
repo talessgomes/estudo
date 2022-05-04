3802a320cd4b8aa1bdaf42edb4859e5115ace6ba
talessgomes <tales.gomes_sl@hotmail.com>
2022-05-04 15:06:21 -0300

    parte-do-curso-concluida

--- ws-eclipse/Seção 15/SaqueConta/src/model/exception/BusinessException.java
+++ ws-eclipse/Seção 15/SaqueConta/src/model/exception/BusinessException.java
@@ -1,6 +1,9 @@
 package model.exception;
 
-public class BusinessException {
-	
+public class BusinessException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
 
+	public BusinessException(String msg) {
+		super(msg);
+	}
 }
